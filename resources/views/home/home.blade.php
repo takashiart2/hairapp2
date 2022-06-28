74d09b40f0591f23999f09f9c49968bdd8d7d110
Takashiart2 <1603taka@gmail.com>
2022-06-28 06:31:06 +0000

    hair0628

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -4,10 +4,11 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Hairapp</title>
+        <title>HairApp</title>
+        <link rel="stlylesheet" href="css/reset.css">
+        <link rel="stlylesheet" href="css/style.css">
     </head>
     <body>
-        <h1>Hairappのホーム画面</h1>
+        <h1>HairApp</h1>
     </body>
 </html>
