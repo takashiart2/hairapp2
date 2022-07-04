2e981f95aeeba7b9cd79ba1238c125be0cc51ea8
Takashiart2 <1603taka@gmail.com>
2022-07-04 11:32:40 +0000

    standard-layout01

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -5,8 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>HairApp</title>
-        <link rel="stlylesheet" href="css/reset.css">
-        <link rel="stlylesheet" href="css/style.css">
+        <!-- Styles -->
+        <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     </head>
     <body>
         <header class="header">
